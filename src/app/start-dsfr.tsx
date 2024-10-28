9e9e63f0571136b95d6c5c74915ddb00141a9b19
Théophile Merlière <theophile.merliere@gmail.com>
2024-10-28 11:09:16 +0100

    Install DSFR

---
+++ src/app/start-dsfr.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import {startReactDsfr} from '@codegouvfr/react-dsfr/next-appdir'
+import Link from 'next/link'
+
+import {defaultColorScheme} from './default-color-scheme.ts'
+
+declare module '@codegouvfr/react-dsfr/next-appdir' {
+  type RegisterLink = {
+    Link: typeof Link;
+  }
+}
+
+startReactDsfr({defaultColorScheme, Link})
+
+export const StartDsfr = () =>
+  // Yes, leave null here.
+  null
+
