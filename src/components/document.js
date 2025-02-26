a1f7b97dc1e786abcbac20e8130f9a28dd30e202
Guillaume <guillaume@livingdata.co>
2025-02-26 11:47:16 +0100

    Utilisation de la fonction formatDate

--- src/components/document.js
+++ src/components/document.js
@@ -1,6 +1,7 @@
 import {Article} from '@mui/icons-material'
 import {Box, Typography} from '@mui/material'
-import {format} from 'date-fns'
+
+import formatDate from '@/lib/format-date.js'
 
 const API_URL = process.env.NEXT_PUBLIC_STORAGE_URL
 
@@ -16,11 +17,11 @@ const Document = ({document}) => (
     <Box sx={{display: 'flex', flexDirection: 'column', p: 1}}>
       <Typography sx={{pr: 1}}>
         <Article sx={{pr: 1, verticalAlign: 'bottom'}} />
-        {document.nature} - {document.reference} du {format(document.date_signature, 'dd/MM/yyyy')}
+        {document.nature} - {document.reference} du {formatDate(document.date_signature)}
       </Typography>
       {document.date_fin_validite && (
         <Typography variant='caption' sx={{pl: 2}}>
-          <i>(Fin de validité : {format(document.date_fin_validite, 'dd/MM/yyyy')})</i>
+          <i>(Fin de validité : {formatDate(document.date_fin_validite)})</i>
         </Typography>
       )}
       {document.remarque && (
