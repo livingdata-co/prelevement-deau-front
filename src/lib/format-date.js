03b7e535f6827f19491fd00f53c2b98de8751a6b
Guillaume <guillaume@livingdata.co>
2025-02-27 15:36:33 +0100

    Ajout transformation "1" en "1er"

--- src/lib/format-date.js
+++ src/lib/format-date.js
@@ -1,6 +1,14 @@
 import {format} from 'date-fns'
 import {fr} from 'date-fns/locale'
 
+function transformFrenchFirst(day) {
+  if (day === 1) {
+    return '1er'
+  }
+
+  return day
+}
+
 function formatDate(date) {
   if (!date) {
     return null
@@ -9,12 +17,15 @@ function formatDate(date) {
   return format(date, 'dd/MM/yyyy')
 }
 
-export function formatPeriodeDate(date) {
-  if (!date) {
+export function formatPeriodeDate(dateString) {
+  if (!dateString) {
     return null
   }
 
-  return format(date, 'd MMMM', {locale: fr})
+  const date = new Date(dateString)
+  const day = date.getDate()
+
+  return `${transformFrenchFirst(day)} ${format(date, 'MMMM', {locale: fr})}`
 }
 
 export default formatDate
