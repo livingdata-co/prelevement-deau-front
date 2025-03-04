09a195b57954bbbd64f08c9fdddd44573bd27790
Théophile Merlière <theophile.merliere@gmail.com>
2025-03-04 13:48:05 +0100

    Remove unused functions

--- src/lib/points-prelevement.js
+++ src/lib/points-prelevement.js
@@ -52,65 +52,6 @@ export function createPointPrelevementFeatures(points) {
   }
 }
 
-export function donutSegment(start, end, r, r0, color) {
-  if (end - start === 1) {
-    end -= 0.000_01
-  }
-
-  const a0 = 2 * Math.PI * (start - 0.25)
-  const a1 = 2 * Math.PI * (end - 0.25)
-  const x0 = Math.cos(a0)
-  const y0 = Math.sin(a0)
-  const x1 = Math.cos(a1)
-  const y1 = Math.sin(a1)
-  const largeArc = end - start > 0.5 ? 1 : 0
-
-  return [
-    '<path d="M',
-    r + (r0 * x0),
-    r + (r0 * y0),
-    'L',
-    r + (r * x0),
-    r + (r * y0),
-    'A',
-    r,
-    r,
-    0,
-    largeArc,
-    1,
-    r + (r * x1),
-    r + (r * y1),
-    'L',
-    r + (r0 * x1),
-    r + (r0 * y1),
-    'A',
-    r0,
-    r0,
-    0,
-    largeArc,
-    0,
-    r + (r0 * x0),
-    r + (r0 * y0),
-    `" fill="${color}" />`
-  ].join(' ')
-}
-
-export function highlightPoint(map, layerId, pointId) {
-  map.setPaintProperty(layerId, 'circle-stroke-color', [
-    'case',
-    ['==', ['get', 'id_point'], pointId],
-    'hotpink',
-    'black'
-  ])
-
-  map.setPaintProperty(layerId, 'circle-stroke-width', [
-    'case',
-    ['==', ['get', 'id_point'], pointId],
-    3,
-    1
-  ])
-}
-
 export const usageColors = {
   'Camion citerne': '#8a2be2',
   'Eau potable': '#007cbf',
