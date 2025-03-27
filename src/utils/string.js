51d9377c4a736b61ad056f85e4705f7f7d6cbe4d
Guillaume <guillaume@livingdata.co>
2025-03-27 11:23:15 +0100

    Adds function to clean name

--- src/utils/string.js
+++ src/utils/string.js
@@ -7,3 +7,13 @@ export function normalizeString(string) {
 
   return deburr(string.toLowerCase())
 }
+
+export function normalizeName(string) {
+  if (!string) {
+    return string
+  }
+
+  return string.split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ')
+}
