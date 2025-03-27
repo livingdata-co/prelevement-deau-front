a991488a0be5b34a9c7a32f7c61f28e3cbe9380c
Guillaume <guillaume@livingdata.co>
2025-03-27 16:27:26 +0100

    Use pointLoader in page

--- src/app/prelevements/[id]/[tab]/page.js
+++ src/app/prelevements/[id]/[tab]/page.js
@@ -1,39 +1,11 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import Link from 'next/link'
 
-import {
-  getBeneficiaire,
-  getBnpe,
-  getBss,
-  getExploitationsFromPointId,
-  getLibelleCommune,
-  getPointPrelevement
-} from '@/app/api/points-prelevement.js'
-import PointExploitations from '@/components/prelevements/point-exploitations.js'
-import PointIdentification from '@/components/prelevements/point-identification.js'
-import PointLocalistation from '@/components/prelevements/point-localisation.js'
-import PointTabs from '@/components/prelevements/point-tabs.js'
+import PointLoader from '@/components/prelevements/point-loader.js'
 
 const Page = async ({params}) => {
   const {id, tab} = (await params)
   const selectedTab = tab || 'identification'
-  const pointPrelevement = await getPointPrelevement(id)
-  const bss = await getBss(pointPrelevement.id_bss)
-  const bnpe = await getBnpe(pointPrelevement.code_bnpe)
-  const commune = await getLibelleCommune(pointPrelevement.insee_com)
-  const exploitations = await getExploitationsFromPointId(pointPrelevement.id_point)
-  const exploitationsWithBeneficiaires = await Promise.all(exploitations.map(async exploitation => {
-    const beneficiaire = await getBeneficiaire(exploitation.id_beneficiaire)
-    return {
-      ...exploitation,
-      beneficiaire
-    }
-  }))
-
-  pointPrelevement.lienBss = bss?.lien_infoterre || ''
-  pointPrelevement.lienBnpe = bnpe?.uri_ouvrage || ''
-  pointPrelevement.libelleCommune = commune?.nom || ''
-  pointPrelevement.exploitations = exploitationsWithBeneficiaires
 
   return (
     <>
@@ -42,22 +14,10 @@ const Page = async ({params}) => {
         <Link href={`/prelevements?point-prelevement=${id}`}>Retour</Link>
       </div>
       <div className='fr-container mt-4'>
-        <PointTabs selectedTab={selectedTab} />
-        {selectedTab === 'identification' && (
-          <PointIdentification
-            pointPrelevement={pointPrelevement}
-          />
-        )}
-        {selectedTab === 'localisation' && (
-          <PointLocalistation
-            pointPrelevement={pointPrelevement}
-          />
-        )}
-        {selectedTab === 'exploitations' && (
-          <PointExploitations
-            pointPrelevement={pointPrelevement}
-          />
-        )}
+        <PointLoader
+          id={id}
+          selectedTab={selectedTab}
+        />
       </div>
     </>
   )
