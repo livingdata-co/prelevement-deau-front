b6f93624ecd22749e709e4b98485f64c083d6df4
Guillaume <guillaume@livingdata.co>
2025-03-27 16:27:07 +0100

    Adds pointLoader component

---
+++ src/components/prelevements/point-loader.js
@@ -0,0 +1,78 @@
+'use client'
+
+import {useState, useEffect} from 'react'
+
+import {
+  getBeneficiaire,
+  getBnpe,
+  getBss,
+  getExploitationsFromPointId,
+  getLibelleCommune,
+  getPointPrelevement
+} from '@/app/api/points-prelevement.js'
+import LoadingOverlay from '@/components/loading-overlay.js'
+import PointExploitations from '@/components/prelevements/point-exploitations.js'
+import PointIdentification from '@/components/prelevements/point-identification.js'
+import PointLocalistation from '@/components/prelevements/point-localisation.js'
+import PointTabs from '@/components/prelevements/point-tabs.js'
+
+const PointLoader = ({id, selectedTab}) => {
+  const [loading, setLoading] = useState(true)
+  const [pointPrelevement, setPointPrelevement] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const pointData = await getPointPrelevement(id)
+        const bss = await getBss(pointData.id_bss)
+        const bnpe = await getBnpe(pointData.code_bnpe)
+        const commune = await getLibelleCommune(pointData.insee_com)
+        const exploitations = await getExploitationsFromPointId(pointData.id_point)
+
+        const exploitationsWithBeneficiaires = await Promise.all(
+          exploitations.map(async exploitation => {
+            const beneficiaire = await getBeneficiaire(exploitation.id_beneficiaire)
+            return {
+              ...exploitation,
+              beneficiaire
+            }
+          })
+        )
+
+        pointData.lienBss = bss?.lien_infoterre || ''
+        pointData.lienBnpe = bnpe?.uri_ouvrage || ''
+        pointData.libelleCommune = commune?.nom || ''
+        pointData.exploitations = exploitationsWithBeneficiaires
+
+        setPointPrelevement(pointData)
+        setLoading(false)
+      } catch (error) {
+        console.error('Erreur lors du chargement des données :', error)
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [id])
+
+  if (loading) {
+    return <LoadingOverlay />
+  }
+
+  return (
+    <>
+      <PointTabs selectedTab={selectedTab} />
+      {selectedTab === 'identification' && (
+        <PointIdentification pointPrelevement={pointPrelevement} />
+      )}
+      {selectedTab === 'localisation' && (
+        <PointLocalistation pointPrelevement={pointPrelevement} />
+      )}
+      {selectedTab === 'exploitations' && (
+        <PointExploitations pointPrelevement={pointPrelevement} />
+      )}
+    </>
+  )
+}
+
+export default PointLoader
