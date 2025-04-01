b9ca53494cb8a36edb559cea7d4415675e6de1e0
Guillaume <guillaume@livingdata.co>
2025-04-01 11:21:23 +0200

    Adds default loading

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+import {Box, CircularProgress} from '@mui/material'
+
+const Loading = () => (
+  <Box className='flex w-full h-full justify-center items-center'>
+    <CircularProgress />
+  </Box>
+)
+
+export default Loading
