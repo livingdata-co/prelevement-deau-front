30521e5330cf2e61a812e8ea12c29e8f55c3fce0
Guillaume <guillaume@livingdata.co>
2025-04-01 14:55:26 +0200

    Removes Suspense & fix loading height

--- src/app/prelevements/[id]/layout.js
+++ src/app/prelevements/[id]/layout.js
@@ -1,14 +1,10 @@
-import {Suspense} from 'react'
-
 import PointTabs from '@/components/prelevements/point-tabs.js'
 
 const Layout = ({children}) => (
   <>
     <PointTabs />
-    <div className='fr-container'>
-      <Suspense>
-        {children}
-      </Suspense>
+    <div className='fr-container h-full'>
+      {children}
     </div>
   </>
 )
--- src/app/prelevements/[id]/loading.js
+++ src/app/prelevements/[id]/loading.js
@@ -1,7 +1,7 @@
 import {Box, CircularProgress} from '@mui/material'
 
 const Loading = () => (
-  <Box className='flex w-full justify-center'>
+  <Box className='flex w-full h-full justify-center items-center'>
     <CircularProgress />
   </Box>
 )
