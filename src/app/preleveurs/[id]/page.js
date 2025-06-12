b8559a94bf80fff2c6b1c384681f987b6af7fd01
Théophile Merlière <theophile.merliere@gmail.com>
2025-06-12 23:33:09 +0200

    Secure preleveur data reading

--- src/app/preleveurs/[id]/page.js
+++ src/app/preleveurs/[id]/page.js
@@ -1,4 +1,6 @@
-import {Box, Chip, Typography} from '@mui/material'
+import {
+  Alert, Box, Chip, Typography
+} from '@mui/material'
 import Link from 'next/link'
 
 import {getPreleveur, getPointsFromPreleveur} from '@/app/api/points-prelevement.js'
@@ -15,34 +17,47 @@ const Page = async ({params}) => {
       <Typography variant='h4' className='fr-mt-3w'>
         {preleveur.civilite} {preleveur.nom} {preleveur.prenom} {preleveur.sigle} {preleveur.raison_sociale}
         <p className='italic'>
-          {preleveur.exploitations.length} {preleveur.exploitations.length === 1
-            ? 'exploitation'
-            : 'exploitations'}
+          {preleveur.exploitations && preleveur.exploitations.length > 0 ? (
+            `${preleveur.exploitations.length} ${preleveur.exploitations.length === 1 ? 'exploitation' : 'exploitations'}`
+          ) : (
+            <Alert severity='info'>Aucune exploitation</Alert>
+          )}
         </p>
       </Typography>
       <div className='italic'>
         <LabelValue label='Usages'>
-          {preleveur.usages && preleveur.usages.length > 0 && preleveur.usages.map(u => (
-            <Chip
-              key={`${u}`}
-              label={u}
-              sx={{
-                ml: 1,
-                backgroundColor: getUsagesColors(u)?.color,
-                color: getUsagesColors(u)?.textColor
-              }}
-            />
-          ))}
+          {preleveur.usages && preleveur.usages.length > 0 ? (
+
+            preleveur.usages.map(u => (
+              <Chip
+                key={`${u}`}
+                label={u}
+                sx={{
+                  ml: 1,
+                  backgroundColor: getUsagesColors(u)?.color,
+                  color: getUsagesColors(u)?.textColor
+                }}
+              />
+            ))
+          ) : (
+            <Alert severity='info'>Aucun usage</Alert>
+          )}
         </LabelValue>
       </div>
       <div><b>Points de prélevement : </b>
-        {points.map(point => (
-          <div key={point.id_point}>
-            <Link href={`/prelevements/${point.id_point}`}>
-              {point.id_point} - {point.nom}
-            </Link>
-          </div>
-        ))}
+        {points && points.length > 0 ? (
+          points.map(point => (
+            <div key={point.id_point}>
+              <Link href={`/prelevements/${point.id_point}`}>
+                {point.id_point} - {point.nom}
+              </Link>
+            </div>
+          ))
+        ) : (
+          <Alert severity='info' className='mt-4'>
+            Aucun point de prélevement
+          </Alert>
+        )}
       </div>
     </Box>
   )
